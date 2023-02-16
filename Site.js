5b4b92ef605deb968cf1c902caf38216b5b2bd27
soklota <soklota@email.sc.edu>
2023-02-16 16:14:22 -0500

    code refresh

---
+++ Site.js
@@ -0,0 +1,16 @@
+function Site() {
+    return (
+        <div className = "Site">
+            <div id="content">
+                <intro></intro>
+                <personalInfo></personalInfo>
+                <workInfo></workInfo>
+                <artProjects></artProjects>
+                <games></games>
+                <apps></apps>
+                <conclusion></conclusion>
+                </div>
+
+        </div>
+    )
+}
